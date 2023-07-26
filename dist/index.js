0b4384927ed3c9898272595ca1dea3c4283f0aee
cc <31040856+CCleanerShot@users.noreply.github.com>
2023-07-26 11:53:56 -0500

    reorrganized files

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const utils_1 = __importDefault(require("./common/utils"));
+const url = "https://www.amazon.com/Anker-PowerCore-Technology-High-Capacity-Compatible/dp/B07S829LBX/ref=sr_1_4?crid=1A9378Y50Y1TX&keywords=battery%2Bpacks%2Banker&qid=1690219592&sprefix=battery%2Bpacks%2Banker%2Caps%2C107&sr=8-4&th=1";
+const cssSelector = "#attach-base-product-price";
+utils_1.default.loadConfig();
+utils_1.default.saveConfig();
+console.log("hi!");
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import utils from "./common/utils";
+
+const url = "https://www.amazon.com/Anker-PowerCore-Technology-High-Capacity-Compatible/dp/B07S829LBX/ref=sr_1_4?crid=1A9378Y50Y1TX&keywords=battery%2Bpacks%2Banker&qid=1690219592&sprefix=battery%2Bpacks%2Banker%2Caps%2C107&sr=8-4&th=1"
+const cssSelector = "#attach-base-product-price";
+utils.loadConfig();
+utils.saveConfig();
+
+console.log("hi!");
+
+
