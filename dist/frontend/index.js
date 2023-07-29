184b06eae49f0920771b3f8dab2b2496bcbc01c8
cc <31040856+CCleanerShot@users.noreply.github.com>
2023-07-29 15:34:26 -0500

    i have no idea

--- dist/frontend/index.js
+++ dist/frontend/index.js
@@ -1,17 +1,33 @@
-console.log("top");
-const doFetch = () => {
+const baseURL = "https://98.57.186.27:3000/"
+const paths = "/"
+const headers = {}
+const AxiosInstance = window.axios.create({
+    baseURL: baseURL,
+    timeout: 5000,
+});
+
+const doFetch = (paths = "") => {
     const options = {}
-    fetch("/")
-        .then(res => console.log(res))
-        .catch(rej => console.log("rejected,", rej))
+    
+    AxiosInstance.get(
+        paths, 
+        {headers: headers},
+    ).then((res) => {
+        console.log(res);
+    }).catch(rej => {
+        console.log(rej);
+    })
+
+
 }
 
 const button = document.createElement("button");
 button.innerHTML = "Im a button!";
-button.onclick(doFetch);
+button.onclick = doFetch();
+button.style.border = "1px solid red"
 
 const header = document.getElementById("header");
 header.innerHTML = "New Words!"
 header.appendChild(button);
 
-console.log("Hello!");
+console.log("Hello from frontend!");
